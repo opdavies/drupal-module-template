48bf75ce265e17f79bbe01c5909f41a2c0879a1d
Oliver Davies <oliver@oliverdavies.uk>
2022-09-23 00:20:41 +0100

    feat: add an example log entry
    * Add the `ExamplePageController` as a service.
    * Add `symfony/config` as a dependency so that the Controller can be
      autowired.
    * Inject the logger channel factory service into the Controller and
      write a message to the log.

--- src/Controller/ExamplePageController.php
+++ src/Controller/ExamplePageController.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace Drupal\drupal_module_template\Controller;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
 final class ExamplePageController {
 
+  public function __construct(
+    private LoggerChannelFactoryInterface $logger
+  ) {}
+
   public function __invoke(): array {
+    $this->logger->get('drupal_module_template')->info('Example page viewed.');
+
     return [];
   }
 
