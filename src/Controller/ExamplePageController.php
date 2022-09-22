d171b1724abde3974f5fa101ec09399b8e57a0dc
Oliver Davies <oliver@oliverdavies.uk>
2022-09-22 23:11:13 +0100

    feat: initial commit

---
+++ src/Controller/ExamplePageController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\drupal_module_template\Controller;
+
+final class ExamplePageController {
+
+  public function __invoke(): array {
+    return [];
+  }
+
+}
---
+++ tests/src/Functional/ExamplePageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\drupal_module_template\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ExamplePageTest extends BrowserTestBase {
+
+  public $defaultTheme = 'stark';
+
+  protected static $modules = [
+    // Core.
+    'node',
+
+    // Custom.
+    "drupal_module_template"
+  ];
+
+  /** @test */
+  public function should_load_the_example_page_for_anonymous_users(): void {
+    // Arrange.
+
+    // Act.
+    $this->drupalGet('/example');
+
+    // Assert.
+    $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
+  }
+
+}
