f0d43a44ae7feb11ea04248ebdd1cb66bd0216ce
Oliver Davies <oliver@oliverdavies.uk>
2022-09-23 07:44:26 +0100

    chore: add some text to the example page

--- src/Controller/ExamplePageController.php
+++ src/Controller/ExamplePageController.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Drupal\drupal_module_template\Controller;
 
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 final class ExamplePageController {
+
+  use StringTranslationTrait;
 
   public function __construct(
     private LoggerChannelFactoryInterface $logger
@@ -15,7 +18,12 @@ final class ExamplePageController {
   public function __invoke(): array {
     $this->logger->get('drupal_module_template')->info('Example page viewed.');
 
-    return [];
+    return [
+      '#markup' => $this->t(
+        'This is an example page from the <a href="@url">Drupal Module Template</a>.',
+        ['@url' => 'https://github.com/opdavies/drupal-module-template']
+      ),
+    ];
   }
 
 }
